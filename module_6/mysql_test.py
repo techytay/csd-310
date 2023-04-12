b32dee38399a93a674d9e67e6791cc3127b654c7
Taylor Niemann <niemann.taylor@gmail.com>
2023-04-12 05:06:05 -0500

    Pushing 04/12/23

---
+++ module_6/mysql_test.py
@@ -0,0 +1,35 @@
+# Taylor Niemann 4/12/23 MySQL Movies database connection
+
+# Python imports needed
+import mysql.connector
+from mysql.connector import errorcode
+
+# Database config object
+config = {
+    "user": "root",
+    "password": "Jmp^AndRun815",
+    "host": "127.0.0.1",
+    "database": "movies",
+    "raise_on_warnings": True
+}
+
+# Connection test code
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\n Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print(" The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print(" The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    db.close()
